0ca6de8d331e7bcf69ede76e93641246530fae2c
anusara <anusara.orm@gmail.com>
2018-05-11 16:15:35 +0700

    add command firebase in Loginface

--- app/src/main/java/com/example/anusara/adventuremate/MainLoginActivity.java
+++ app/src/main/java/com/example/anusara/adventuremate/MainLoginActivity.java
@@ -7,36 +7,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainLoginActivity extends AppCompatActivity {
 
-    private FirebaseAnalytics mFirebaseAnalytics;
 
-    private EditText inputEmail, inputPassword;
-    private FirebaseAuth auth;
-    private ProgressBar progressBar;
-    private Button btnSignup, btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //Get Firebase auth instance
-        auth = FirebaseAuth.getInstance();
-
-        if (auth.getCurrentUser() != null) {
-            startActivity(new Intent(MainLoginActivity.this, HomeActivity.class));
-            finish();
-        }
         setContentView(R.layout.activity_main_login);
-
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-
-    }
-
-    public FirebaseAnalytics getmFirebaseAnalytics() {
-        return mFirebaseAnalytics;
     }
 }
